70f248057e3c9e55b462c65c38f6ece231dd2625
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-01-29 14:04:18 -0500

    Services created and bootstrap added

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,9 +1,11 @@
 import courses from './courses.json'
+
 class CourseService {
   constructor() {
     this.courses = courses;
     this.tempcourse = "";
   }
+
   createCourse = course => {
     if(course === null) {
       course = {
@@ -14,18 +16,26 @@ class CourseService {
     this.courses.push(course)
     return this.courses
   }
+
   findCourseById = courseId =>
     this.courses = this.courses.find(
       course => course.id === courseId
     )
+
   findAllCourses = () =>
     this.courses;
+  
   deleteCourse = deleteCourse =>
     this.courses = this.courses.filter(
       course => course.id !== deleteCourse.id
     )
-   updateCourse = (id, course) =>
-      this.deleteCourse(id)
-      this.courses.push(course)
+
+    updateCourse = (id, course) => {
+      for (var i = 0; i < this.courses.length; i++) {
+        if(id === this.courses[i].id){
+          this.courses[i] = course;
+        }
+    }
+  }
 }
 export default CourseService
