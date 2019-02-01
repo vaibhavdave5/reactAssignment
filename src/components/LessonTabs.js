f568269819f730e4e0a0e799fb2e5a777758fdd7
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-02-01 11:49:15 -0500

    WidgetList Seperated

--- src/components/LessonTabs.js
+++ src/components/LessonTabs.js
@@ -1,17 +1,5 @@
 import React from 'react'
 
-// const LessonTabs = ({lessons}) =>
-//   <ul className="nav nav-tabs">
-//     {
-//       lessons.map(lesson =>
-//         <li key={lesson.id} className="nav-item">
-//           <a className="nav-link active"
-//              href="#">{lesson.title}
-//           </a>
-//         </li>
-//       )
-//     }
-//   </ul>
 
 const LessonTabs = ({lesson}) =>
     <li className="nav-item">
