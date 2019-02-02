499df70bca1b1589c842179f060d11f840da3259
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-02-01 20:48:30 -0500

    toggle button

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 const CourseCard = ({course, deleteCourse}) =>
-<div className = "col-sm-12 col-md-4 col-lg-2">
-<div className="card mb-4">
-    <img className="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title">{course.title}</h5>
-      <p className="card-text">Card text.</p>
-      <Link className="btn btn-primary" to={`/course/${course.id}`}>Edit</Link>
-      <a onClick={() => deleteCourse(course)}
-         className="btn btn-danger">Delete</a>
+    <div className = "col-sm-12 col-md-4 col-lg-2">
+        <div className="card mb-4">
+            <img className="card-img-top" src="https://picsum.photos/200/200"
+                 alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">{course.title}</h5>
+                <p className="card-text">Card text.</p>
+                <Link className="btn btn-primary" to={`/course/${course.id}`}>Edit</Link>
+                <a onClick={() => deleteCourse(course)}
+                   className="btn btn-danger">Delete</a>
+            </div>
+        </div>
     </div>
-</div>
-</div>
 export default CourseCard;
--- src/components/CourseListNav.js
+++ src/components/CourseListNav.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-
+import {Link} from 'react-router-dom'
 export default class CourseList extends Component{
-    constructor(props) {super(props);}
+    constructor(props) {
+        super(props);
+        this.state={
+            courseList: 'none',
+            courseGrid: ''
+        }
+    }
+
+    handleClick = () =>
+    {
+        var parts = window.location.href.split('/');
+        var lastSegment = parts.pop() || parts.pop();  // handle potential trailing slash
+        console.log(lastSegment)
+
+        if(lastSegment ==='table'){
+            this.setState({
+                courseList: 'none',
+                courseGrid: ''
+            })
+        }
+        else{
+            this.setState({
+                courseList: '',
+                courseGrid: 'none'
+            })
+        }
+    }
 
     render(){
         return(
             <div>
-                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        
-                <div className="collapse navbar-collapse col-3" 
-                    id="navbarTogglerDemo01">
-                    <a className="navbar-brand" 
-                    href="#">Course Manager</a>
-                </div>
-                <div>
-                <div className="input-group mb-3">
-                    <input type="text" 
-                        className="form-control"
-                        placeholder="New Course Name"
-                        aria-label="Add new course" 
-                        aria-describedby="basic-addon2"
-                        size = '80' />
-                    <div className="input-group-append">
-                    <button className="btn btn-outline-success " 
-                            type="submit">Add</button>
-                    </div>
-                </div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-        </div>
-    </nav>
-    <Link to="/">Course Grid</Link> |
-   <Link to="/table">Course Table</Link>
+                    <div className="collapse navbar-collapse col-3"
+                         id="navbarTogglerDemo01">
+                        <a className="navbar-brand"
+                           href="#">Course Manager</a>
+                    </div>
+                    <div>
+                        <div className="input-group mb-3">
+                            <input type="text"
+                                   className="form-control"
+                                   placeholder="New Course Name"
+                                   aria-label="Add new course"
+                                   aria-describedby="basic-addon2"
+                                   size = '80' />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-success "
+                                        type="submit">Add</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </nav>
+
+                <div style={{display: this.state.courseGrid}}>
+                    <Link onClick={this.handleClick} to="/">
+                        Course Grid</Link></div>
+                <div style={{display: this.state.courseList}} >
+                    <Link to="/table" onClick={this.handleClick}>Course Table</Link></div>
             </div>
         )
     }
-
-
 }
