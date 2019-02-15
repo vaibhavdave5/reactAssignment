d1242be4c35b3814966d389b2abcce1fa2b1caef
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-02-15 17:22:43 -0500

    Module service ready in fe

---
+++ src/services/ModuleService.js
@@ -0,0 +1,116 @@
+
+class ModuleService {
+    constructor() {}
+
+    findAllModules = cid => {
+        var data = JSON.stringify(false);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                return JSON.parse(this.responseText);
+            }
+        });
+
+        xhr.open("GET", "http://localhost:8080/api/course/"+cid+"/modules");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("credentials", "include");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "2ac1d87b-fa9b-400e-b62d-2c97bf43bcfa");
+
+        xhr.send(data);
+    }
+
+    createModule = (cid, module) =>{
+        var data = JSON.stringify({
+            "id": module.id,
+            "chapters": module.lessons
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                return JSON.parse(this.responseText);
+            }
+        });
+
+        xhr.open("POST", "http://localhost:8080/api/courses/"+cid+"/modules");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("credentials", "include");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "045c9dad-6f9a-4463-ba0f-733144d692b5");
+
+        xhr.send(data);
+    }
+
+    findModuleById = mid => {
+        var data = JSON.stringify(false);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("GET", "http://localhost:8080/api/modules/"+mid);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("credentials", "include");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "f8a648e6-8eed-494c-ab34-3e63e42a23c9");
+
+        xhr.send(data);
+    }
+
+    updateModule = (mid, module) => {
+        var data = JSON.stringify({
+            "id": module.id,
+            "chapters": module.lessons
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("PUT", "http://localhost:8080/api/modules/"+mid);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("credentials", "include");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "bd5832e6-e7a2-4de7-bb5e-f645b5636001");
+
+        xhr.send(data);
+    }
+
+    deleteModule = mid => {
+        var data = JSON.stringify(false);
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("DELETE", "http://localhost:8080/api/modules/"+mid);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("credentials", "include");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "e9e8537b-c9dc-4774-bb08-7c3949787218");
+
+        xhr.send(data);
+    }
+}
+export default ModuleService;
